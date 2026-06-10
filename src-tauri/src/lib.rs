ea9d82374f7d3cdf47bbe1481d14ca510d7cfb65
Dan <zouyan27@gmail.com>
2026-06-10 01:48:01 -0700

    fix: register tauri-plugin-dialog so folder picker works

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -16,6 +16,7 @@ use tauri_plugin_global_shortcut::{GlobalShortcutExt, ShortcutState};
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
+        .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_global_shortcut::Builder::new().build())
         .manage(AppState(Mutex::new(VaultState::new())))
         .invoke_handler(tauri::generate_handler![
